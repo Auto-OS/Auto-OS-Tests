c50abba18e0a22253782d8652e895277cccc34a9
Luke Melaia <lukemelaia.personal@gmail.com>
2021-02-24 00:29:19 +0200

    Added gradle project used to test subprojects.

---
+++ test-hotkeys/src/main/java/org/autoos/tests/hotkeys/HotkeysTest.java
@@ -0,0 +1,22 @@
+package org.autoos.tests.hotkeys;
+
+import org.graalvm.polyglot.Context;
+
+/**
+ * The entry point for the Hotkeys test case.
+ */
+public final class HotkeysTest {
+
+    private HotkeysTest() {
+        //Init
+    }
+
+    private void run() {
+        //Start
+    }
+
+    public static void main(String[] args) {
+        HotkeysTest test = new HotkeysTest();
+        test.run();
+    }
+}
